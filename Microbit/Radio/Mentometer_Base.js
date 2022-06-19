800d21dcb8da933073cf03e56756a105c42a01b4
Lars Åge Kamfjord <lars.age@laaknor.no>
2022-06-19 13:01:29 +0200

    Adding Mentometer

---
+++ Microbit/Radio/Mentometer_Base.js
@@ -0,0 +1,34 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    registrerStemme(receivedNumber, radio.receivedPacket(RadioPacketProperty.SerialNumber))
+})
+function registrerStemme (styrke: number, id: number) {
+    velgerindeks = velger.indexOf(id)
+    if (velgerindeks < 0) {
+        velger.push(id)
+        velgerindeks = velger.indexOf(id)
+    }
+    ledX = Math.floor(velgerindeks / 5)
+    ledY = 4 - (velgerindeks - 0 * 5)
+    led.plotBrightness(ledX, ledY, styrke)
+}
+input.onButtonPressed(Button.B, function () {
+    velger = []
+    ledX = 0
+    ledY = 0
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+let ledY = 0
+let ledX = 0
+let velgerindeks = 0
+let velger: number[] = []
+velger = []
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
